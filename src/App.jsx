9befa3da95ed5c7a236bcb56d271e82c95280c7c
Eshanto Ghop Apurbo <eshantoghopapurbo@gmail.com>
2026-03-30 09:15:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2 className='text-4xl bg-red-300 justify-between '>Hello world !</h2>
+    </>
+  )
+}
+
+export default App
