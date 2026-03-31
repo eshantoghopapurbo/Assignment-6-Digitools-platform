1e9784a57dfb4f8abc35364967305fd7d7c65cd4
Eshanto Ghop Apurbo <eshantoghopapurbo@gmail.com>
2026-03-31 18:42:27 +0600

    footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { FaSquareInstagram, FaFacebook, FaXTwitter} from "react-icons/fa6";
 const Footer = () => {
     return (
         <div className='border border-amber-300 bg-[#101727] mt-30'>
-           <div className='container mx-auto flex justify-around my-17'>
+           <div className='container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-10 my-17'>
              <div className=''>
             <h1 className='text-[#FFFFFF] text-3xl font-bold mb-4'>DigiTools</h1>
             <p className='text-[#FFFFFF]'>Premium digital tools for creators,<br/> professionals, and businesses. Work smarter <br/> with our suite of powerful tools.</p>
@@ -32,7 +32,7 @@ const Footer = () => {
             </div>
             <div className='text-[#FFFFFF]'>
                 <h1 className='text-2xl mb-4'>Social Links</h1>
-              <div className='flex gap-9'>
+              <div className='flex gap-6 sm:gap-9 justify-start sm:justify-center lg:justify-start'>
                 <FaSquareInstagram />
               <FaFacebook />
               <FaXTwitter />
@@ -41,7 +41,7 @@ const Footer = () => {
            </div>
            <div className='container mx-auto'>
             <hr className='border-t-2 border-gray-600' />
-            <div className='flex justify-between my-8'>
+            <div className='flex flex-col sm:flex-row justify-between items-center gap-4 my-8'>
                 <p className='text-[#FAFAFA]' >© 2026 Digitools. All rights reserved.</p>
                 <div className='flex gap-10 text-[#FAFAFA] '>
                     <p>Privacy Policy</p>
